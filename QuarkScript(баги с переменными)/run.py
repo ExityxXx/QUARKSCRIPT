64c5fab98f73b9b42619d10452ffad5efc1ef143
exityx <gdlasorum@gmail.com>
2025-02-24 21:18:32 +0300

    Update run.py

--- QuarkScript(баги с переменными)/run.py
+++ QuarkScript(баги с переменными)/run.py
@@ -1,11 +1,10 @@
-# запускает все
 import os
 from sys import *
 
-from Exiscript.parser import *
+from parser import *
 from lexer import *
 from exception import Error, StringError
-
+from interpreter import *
 
 if __name__ == '__main__':
     script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -26,15 +25,19 @@ if __name__ == '__main__':
     if len(content_to_compile) > 0:
         print("CODE: ")
         print(content_to_compile)
-        print("LEXER TOKENS:")
-        for l in lexer_result:
-            print(l)
+        # print("LEXER TOKENS:")
+        # for l in lexer_result:
+        #     print(l)
 
         try:
             parser_result = parse(lexer_result)
             print("PARSER RESULT: ")
             for i in parser_result:
                 print(i)
+            interpreter_result = interpret(parser_result)
+            print("INTERPRETER RESULT:")
+            for result in interpreter_result:
+                print(result)
         except Error as e:
             print(f"{e}")
             exit(1)
@@ -42,5 +45,7 @@ if __name__ == '__main__':
         except Exception as e:
             print(f"Unexpected error: {e}")
             exit(1)
+
+
     else:
         print("EMPTY")
