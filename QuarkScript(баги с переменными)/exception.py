5f4d7b54436a0fc9141a1bebcbb448161c6072cf
exityx <gdlasorum@gmail.com>
2025-03-09 13:49:34 +0300

    Update exception.py

--- QuarkScript(баги с переменными)/exception.py
+++ QuarkScript(баги с переменными)/exception.py
@@ -1,3 +1,4 @@
+
 class Error(BaseException):
     def __init__(self, error_name, details, code_line, column):
         self.error_name = error_name
@@ -20,3 +21,9 @@ class StringError(Error):
     def __str__(self):
         return super().__str__()
 
+class InterpreterErrors(Exception):
+    def __init__(self, error_name, details):
+        self.error_name = error_name
+        self.details = details
+    def __str__(self):
+        return f"{self.error_name}: {self.details}"
