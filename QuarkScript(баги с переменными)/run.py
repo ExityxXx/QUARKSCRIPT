0deaa7980993b57730e0a36487aa2840077a85d5
exityx <gdlasorum@gmail.com>
2025-03-09 13:47:32 +0300

    Update run.py

--- QuarkScript(баги с переменными)/run.py
+++ QuarkScript(баги с переменными)/run.py
@@ -1,11 +1,14 @@
 import os
 from sys import *
 
+import colorama
+
 from parser import *
 from lexer import *
-from exception import Error, StringError
+from exception import *
 from interpreter import *
-
+from colorama import Fore, init, Style
+init()
 if __name__ == '__main__':
     script_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(script_dir, "index.qs")
@@ -24,28 +27,33 @@ if __name__ == '__main__':
     lexer_result = run(content_to_compile)
     if len(content_to_compile) > 0:
         print("CODE: ")
-        print(content_to_compile)
-        # print("LEXER TOKENS:")
-        # for l in lexer_result:
-        #     print(l)
+        print(f"    {content_to_compile}")
+        print("LEXER TOKENS:")
+        for l in lexer_result:
+            print(f"    {l}")
 
         try:
             parser_result = parse(lexer_result)
             print("PARSER RESULT: ")
             for i in parser_result:
-                print(i)
+                print(f"    {i}")
             interpreter_result = interpret(parser_result)
             print("INTERPRETER RESULT:")
             for result in interpreter_result:
-                print(result)
+                if result is not None:
+                    print(result)
+        except InvalidCharacterError as inv:
+            print(f"{Fore.RED}{colorama.Style.BRIGHT}{inv}{Fore.RESET}")
+            exit(1)
         except Error as e:
-            print(f"{e}")
+            print(f"{Fore.RED}{colorama.Style.BRIGHT}{e}{Fore.RESET}")
             exit(1)
-
-        except Exception as e:
-            print(f"Unexpected error: {e}")
+        except InterpreterErrors as ie:
+            print(f"{Fore.RED}{colorama.Style.BRIGHT}{ie}{Fore.RESET}")
             exit(1)
-
+        # except Exception as e:
+        #     print(f"{Fore.RED}{Style.BRIGHT}Unexpected Error: {e}{Fore.RESET}")
+        #     exit(1)
 
     else:
         print("EMPTY")
